32b8c248053bf194504dffecdd8e7d56ddfd368a
santushtisharma10 <sharmasantushti1012@gmail.com>
2023-06-10 00:34:32 +0530

    [modification]: minor changes added

--- frontend/src/components/Home/Features.tsx
+++ frontend/src/components/Home/Features.tsx
@@ -1,12 +1,14 @@
 import { Box, styled, Typography } from "@mui/material";
 
 const FeatureSection = styled(Box)({
-  padding: "60px"
+  padding: "60px",
+  width: "100%",
+  color: "#2b3c4d",
 })
 export default function Features() {
   return (
     <FeatureSection>
-      <Typography variant="h2" fontWeight={600}>Features</Typography>
+      <Typography variant="h4" fontWeight={600} align="center">Features</Typography>
     </FeatureSection>
   )
 }
--- frontend/src/components/Home/Working.tsx
+++ frontend/src/components/Home/Working.tsx
@@ -1,15 +1,18 @@
 import { Box, styled, Typography } from "@mui/material";
 
 const WorkSection = styled(Box)({
-  padding: "100px",
+  padding: "60px",
   width: "100%",
-  color: "#2b3c4d"
+  color: "#2b3c4d",
 });
 
 export default function Working() {
   return (
     <WorkSection>
-      <Typography variant="h2">Working</Typography>
+      <Typography variant="h4" fontWeight={600} align="center">How we work ?</Typography>
+      <Box sx={{display: 'grid', gridTemplateColumns: "repeat(2, 1fr)", gap: "40px"}}>
+        
+      </Box>
     </WorkSection>
   )
 }
