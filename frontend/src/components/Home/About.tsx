29ade9acfcfca2aaad51f61a1f81ba8e773dd019
santushtisharma10 <sharmasantushti1012@gmail.com>
2023-06-10 00:33:38 +0530

    [modification]: about page modified

--- frontend/src/components/Home/About.tsx
+++ frontend/src/components/Home/About.tsx
@@ -1,17 +1,25 @@
 import { Box, styled, Typography } from "@mui/material";
+import img from "../../assets/chatbot-bg.png"
 
 const AboutSection = styled(Box)({
-  padding: "100px",
+  padding: "100px 200px 20px 200px",
   backgroundColor: "#ffebe6",
   width: "100%",
-  display: "flex",
-  justifyContent: "center",
-  color: "#2b3c4d"
+  color: "#2b3c4d",
+  textAlign: 'center'
 });
 export default function About() {
   return (
     <AboutSection>
-      <Typography variant="h2" fontWeight={600}>About Section</Typography>
+      <Typography variant="h2" fontWeight={800}>
+        Make customer engagement easy and fun!
+      </Typography>
+      <Typography variant="h6" mt={4} color="#304457">
+        Integrate human-like conversations without human interaction, no code.
+        Turn your knowledge base into a customized chatbot AI chatbot builder is
+        not your regular AI bot platform. 
+      </Typography>
+      <img src={img} width="60%" />
     </AboutSection>
   );
 }
