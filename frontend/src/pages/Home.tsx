bb5a44cf2948dc4a43a28858428942f100648a2c
santushtisharma10 <sharmasantushti1012@gmail.com>
2023-06-09 15:39:09 +0530

    [modification]: minor changes

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,13 @@
+import FAQ from "../components/Home/FAQ";
 import Hero from "../components/Home/Hero";
 import Navbar from "../components/Home/Navbar";
 
 export default function Home() {
   return (
-    <div>
+    <div className="container">
       <Navbar />
       <Hero />
+      <FAQ />
     </div>
   );
 }
