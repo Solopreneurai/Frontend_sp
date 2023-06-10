03711287d7c59ac0f89e25c241ac75fc492b9060
santushtisharma10 <sharmasantushti1012@gmail.com>
2023-06-10 15:46:13 +0530

    [modification]: demo section modified

--- frontend/src/components/Home/Working.tsx
+++ frontend/src/components/Home/Working.tsx
@@ -1,18 +1,28 @@
 import { Box, styled, Typography } from "@mui/material";
+import DemoImg from "../../assets/demo.png"
 
 const WorkSection = styled(Box)({
   padding: "60px",
   width: "100%",
   color: "#2b3c4d",
+  display: "flex",
+  alignItems: "center",
+  flexDirection: "column",
 });
 
 export default function Working() {
   return (
-    <WorkSection>
-      <Typography variant="h4" fontWeight={600} align="center">How we work ?</Typography>
-      <Box sx={{display: 'grid', gridTemplateColumns: "repeat(2, 1fr)", gap: "40px"}}>
-        
+    <WorkSection sx={{pb: "0 !important"}}>
+      <Box maxWidth="600px">
+        <Typography variant="h4" fontWeight={600} align="center" mb={2}>
+          How we work ?
+        </Typography>
+        <Typography variant="body1" align="center" fontWeight={500}>
+          We've simplified all the challenging stages for you. In just a few
+          easy steps, create and train a chatbot for your website.
+        </Typography>
       </Box>
+      <img src={DemoImg} alt="demo-img" width="100%" height="600px" />
     </WorkSection>
-  )
+  );
 }
