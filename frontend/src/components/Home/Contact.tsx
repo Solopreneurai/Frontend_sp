50606090175afa2206e2507af68b935fd915eb5f
santushtisharma10 <sharmasantushti1012@gmail.com>
2023-06-11 17:04:06 +0530

    [fix]: page overflow fixed

--- frontend/src/components/Home/Contact.tsx
+++ frontend/src/components/Home/Contact.tsx
@@ -1,6 +1,6 @@
 import { Box, styled, Typography } from "@mui/material";
 import Img from "../../assets/contact-img.png";
-import { CustomBox, StartButton, RequestButton } from "./Hero"
+import { CustomBox, StartButton, RequestButton } from "./Hero";
 import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -8,18 +8,19 @@ import "aos/dist/aos.css";
 const ContactSection = styled(Box)({
   padding: "60px",
   display: "grid",
-  gridTemplateColumns: 'repeat(2, 1fr)',
+  gridTemplateColumns: "repeat(2, 1fr)",
   color: "#2b3c4d",
-  gap: '30px'
+  gap: "30px",
+  overflowX: "hidden",
 });
 
 export default function Contact() {
-  useEffect(()=>{
-    AOS.init()
-  },[])
+  useEffect(() => {
+    AOS.init();
+  }, []);
   return (
     <ContactSection id="contact">
-      <Box maxWidth='500px' data-aos="fade-right" data-aos-duration="3000">
+      <Box maxWidth="500px" data-aos="fade-right" data-aos-duration="3000">
         <Typography align="left" variant="h2" fontWeight={800} mb={6}>
           Contact us
         </Typography>
@@ -31,10 +32,10 @@ export default function Contact() {
           Effortlessly connect your Popupsmart account with your preferred
           service provider to maximize website conversions.
         </Typography>
-        <CustomBox style={{justifyContent: 'flex-start'}}>
-        <StartButton variant="contained">Book Meeting</StartButton>
-        <RequestButton variant="outlined">Chat With Us</RequestButton>
-      </CustomBox>
+        <CustomBox style={{ justifyContent: "flex-start" }}>
+          <StartButton variant="contained">Book Meeting</StartButton>
+          <RequestButton variant="outlined">Chat With Us</RequestButton>
+        </CustomBox>
       </Box>
       <Box textAlign="center" data-aos="fade-left" data-aos-duration="3000">
         <img src={Img} alt="alt" width="80%" />
