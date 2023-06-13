9428a75e6f76dffbb60d89b13fbb374d7e08d49b
santushtisharma10 <sharmasantushti1012@gmail.com>
2023-06-14 00:06:09 +0530

    [modification]: routes modified

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,29 +1,29 @@
 import { lazy } from 'react';
-import { useRoutes } from "react-router-dom"
+import { RouteObject, useRoutes } from "react-router-dom"
 
 const Portal = lazy(() => import('./pages/Portal'))
 const Home = lazy(() => import('./pages/Home'))
 
+const routes : RouteObject[] = [
+    {
+        path: "/",
+        children: [
+            {
+                index: true,
+                element: <Home />
+            }
+        ]
+    },
+    {
+        path: "portal",
+        children: [
+            {
+                index: true,
+                element: <Portal />
+            }
+        ]
+    }
+]
 export default function Router() {
-    return useRoutes([
-        {
-            path: "/",
-            children: [
-                {
-                    index: true,
-                    element: <Home />
-                }
-            ]
-        },
-        {
-            path: "portal",
-            children: [
-                {
-                    index: true,
-                    element: <Portal />
-                }
-            ]
-        }
-    ])
-   
+    return useRoutes(routes)
 }
