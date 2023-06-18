675ad131e6d797e1616e089fca6c783de1c4fdf2
k-art-i-k <sharmasantushti1012@gmail.com>
2023-06-18 22:12:18 +0530

    [modification]: preview section added

--- frontend/src/pages/Bot.tsx
+++ frontend/src/pages/Bot.tsx
@@ -1,9 +1,17 @@
-import { Box, Tab, styled, Typography, Divider, Avatar } from "@mui/material";
+import { Box, Tab, styled, Typography, Divider, Avatar, IconButton } from "@mui/material";
 import { useState } from "react";
 import { useLocation } from "react-router-dom";
 import TabContext from "@mui/lab/TabContext";
 import TabList from "@mui/lab/TabList";
 import TabPanel from "@mui/lab/TabPanel";
+import Source from "../components/Portal/Builder/Source";
+import Customize from "../components/Portal/Builder/Customize";
+import UserData from "../components/Portal/Builder/UserData";
+import Starter from "../components/Portal/Builder/Starter";
+import Conversation from "../components/Portal/Builder/Conversation";
+import Embeddings from "../components/Portal/Builder/Embeddings";
+import Analytics from "../components/Portal/Builder/Analytics";
+import { Fullscreen } from '@mui/icons-material/';
 
 const Header = styled(Box)({
   padding: "30px 30px 0px 30px",
@@ -12,64 +20,76 @@ const Header = styled(Box)({
   overflow: "scroll",
 });
 const TabStyle = styled(Tab)({
-  color: "#2b3c4d",
-  textTransform: 'none',
-  fontWeight: 600,
+  color: "var(--main-color)",
+  textTransform: "none",
   "&.MuiButtonBase-root.MuiTab-root.Mui-selected": {
-    color: "#ff5c35",
+    color: "var(--secondary-color)",
   },
 });
 const ContentWrapper = styled(Box)({
-  padding: "20px 10px",
+  display: "flex",
+  height: '100%',
+});
+const PreviewBox = styled(Box)({
+  background: "var(--light-blue)",
+  padding: "20px",
+  flex: 1,
+  maxWidth: 450
 });
 
-function Bot() {
+export default function Bot() {
   const [value, setValue] = useState("1");
   const handleChange = (e: React.SyntheticEvent, val: string) => {
-    console.log(e);
+    console.log(e)
     setValue(() => val);
   };
   const { bot } = useLocation().state;
+  const data = [
+    { id: "1", label: "Source", element: <Source /> },
+    { id: "2", label: "Customize", element: <Customize /> },
+    { id: "3", label: "Starter Questions", element: <Starter /> },
+    { id: "4", label: "User Data", element: <UserData /> },
+    { id: "5", label: "Conversations", element: <Conversation /> },
+    { id: "6", label: "Embeddings", element: <Embeddings /> },
+    { id: "7", label: "Analytics", element: <Analytics /> },
+  ];
 
   return (
-    <div>
+    <div style={{height: '100%'}}>
       <TabContext value={value}>
         <Header>
           <Typography variant="h4" fontWeight={600}>
             {bot?.name}{" "}
           </Typography>
           <TabList
-            TabIndicatorProps={{ style: { background: "#ff5c35" } }}
+            TabIndicatorProps={{
+              style: { background: "var(--secondary-light-color)"},
+            }}
             onChange={handleChange}
             variant="scrollable"
             scrollButtons={false}
           >
-            <TabStyle label="Source" value="1" />
-            <TabStyle label="Customize" value="2" />
-            <TabStyle label="Starter Questions" value="3" />
-            <TabStyle label="User Data" value="4" />
-            <TabStyle label="Conversations" value="5" />
-            <TabStyle label="Embeddings" value="6" />
-            <TabStyle label="Analytics" value="7" />
-            <TabStyle label="Other" value="8" />
+            {data.map((item) => (
+              <TabStyle key={item.id} label={item.label} value={item.id} />
+            ))}
           </TabList>
 
-          <Avatar sx={{ bgcolor: "#ff5c35" }}>N</Avatar>
+          <Avatar sx={{ bgcolor: "var(--secondary-color)" }}>N</Avatar>
         </Header>
         <Divider />
         <ContentWrapper>
-          <TabPanel value="1">Source</TabPanel>
-          <TabPanel value="2">Customize</TabPanel>
-          <TabPanel value="3">Starter Questions</TabPanel>
-          <TabPanel value="4">user data</TabPanel>
-          <TabPanel value="5">Converstations</TabPanel>
-          <TabPanel value="6">Embeddings</TabPanel>
-          <TabPanel value="7">Analytics </TabPanel>
-          <TabPanel value="8">others</TabPanel>
+          {data.map((item) => (
+            <TabPanel style={{overflow: 'auto', flex: 1}} key={item.id} value={item.id}>
+              {item.element}
+            </TabPanel>
+          ))}
+          <PreviewBox>
+            <IconButton sx={{p: 0}}>
+              <Fullscreen />
+            </IconButton>
+          </PreviewBox>
         </ContentWrapper>
       </TabContext>
     </div>
   );
 }
-
-export default Bot;
