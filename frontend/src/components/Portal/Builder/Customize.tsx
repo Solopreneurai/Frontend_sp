31a19808fa60a8e9c648bb90a98081ea391d19f5
k-art-i-k <sharmasantushti1012@gmail.com>
2023-06-19 17:25:29 +0530

    [modification]: customize added

--- frontend/src/components/Portal/Builder/Customize.tsx
+++ frontend/src/components/Portal/Builder/Customize.tsx
@@ -1,7 +1,94 @@
-function Customize() {
+import {
+  Box,
+  IconButton,
+  InputLabel,
+  Typography,
+  styled,
+  InputBase,
+} from "@mui/material";
+import { FilledButton } from "../../Home/Hero";
+import { InfoOutlined } from "@mui/icons-material";
+import { customFormData } from "../../../utils/constants";
+
+const Form = styled(Box)({
+  padding: "20px 0",
+  display: "grid",
+  gridTemplateColumns: "repeat(2, 1fr)",
+  gap: "50px",
+});
+
+export const Input = styled(InputBase)({
+  border: "1px solid #cbd6e2",
+  padding: "10px 16px",
+  borderRadius: "10px",
+});
+
+type Props = {
+  name: string;
+};
+function Customize(props: Props) {
   return (
-    <div>Customize</div>
-  )
+    <>
+      <Box className="flex">
+        <Typography variant="h5" fontWeight={600}>
+          Customize
+        </Typography>
+        <FilledButton variant="contained" style={{ padding: "6px 32px" }}>
+          Update
+        </FilledButton>
+      </Box>
+      <Form>
+        <Box>
+          {customFormData.map((data) => (
+            <Box key={data.id}>
+              <InputLabel htmlFor={data.id}>
+                <Box
+                  style={{ display: "flex", gap: "5px", alignItems: "center" }}
+                  mb={0.5}
+                >
+                  <Typography fontSize={12}>{data.label}</Typography>
+                  <IconButton sx={{ p: 0 }}>
+                    <InfoOutlined style={{ fontSize: 14, color: " #cbd6e2" }} />
+                  </IconButton>
+                </Box>
+              </InputLabel>
+              <Input
+                fullWidth
+                placeholder={data.placeholder}
+                defaultValue={data.id === "1" ? props.name : data.defaultValue}
+                id={data.id}
+                sx={{ mb: 2 }}
+              />
+            </Box>
+          ))}
+        </Box>
+        <Box>
+          <InputLabel>
+            <Box
+              style={{ display: "flex", gap: "5px", alignItems: "center" }}
+              mb={0.5}
+            >
+              <Typography fontSize={12}>Company Logo</Typography>
+              <IconButton sx={{ p: 0 }}>
+                <InfoOutlined style={{ fontSize: 14, color: " #cbd6e2" }} />
+              </IconButton>
+            </Box>
+          </InputLabel>
+          <InputLabel>
+            <Box
+              style={{ display: "flex", gap: "5px", alignItems: "center" }}
+              mb={0.5}
+            >
+              <Typography fontSize={12}>Chatbot Picture</Typography>
+              <IconButton sx={{ p: 0 }}>
+                <InfoOutlined style={{ fontSize: 14, color: " #cbd6e2" }} />
+              </IconButton>
+            </Box>
+          </InputLabel>
+        </Box>
+      </Form>
+    </>
+  );
 }
 
-export default Customize
+export default Customize;
