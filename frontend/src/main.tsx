8afc823bf427a821975ab4b071a9cfabcfa100c0
k-art-i-k <sharmasantushti1012@gmail.com>
2023-06-23 17:06:45 +0530

    [modification]: store added in app

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,13 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-// import { BrowserRouter } from "react-router-dom";
 import { HashRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { Store, legacy_createStore as createStore } from "redux";
+import { reducer } from "./store/reducer.tsx";
+
+const store: Store<State, Action> = createStore(reducer);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
+    <Provider store={store}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Provider>
   </React.StrictMode>
 );
