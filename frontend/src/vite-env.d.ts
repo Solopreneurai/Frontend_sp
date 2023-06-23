82f4a5333be4435dd886ecfa8c915421bf5706c5
k-art-i-k <sharmasantushti1012@gmail.com>
2023-06-23 12:56:28 +0530

    [modification]: interfaces and types defined

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -2,3 +2,25 @@
 declare module 'swiper';
 declare module 'aos';
 declare module 'react-router-hash-link'
+interface BotCardDetails {
+    id: string;
+    name: string;
+    folderId: string
+  }
+
+  interface FolderDetails {
+    id: string;
+    name: string;
+  }
+
+  type State = {
+    isUserLoggedIn: boolean;
+    currentFolderId: string;
+    folderList: FolderDetails[]
+    botList: BotCardDetails[]
+  }
+
+  type Action = {
+    type: string;
+    payload: any;
+  }
