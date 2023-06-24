ae69b6f3d6ead049afa6cb6223527adec694e8c5
k-art-i-k <sharmasantushti1012@gmail.com>
2023-06-24 21:20:12 +0530

    [modification]: folder removed

--- frontend/src/components/Portal/Navbar/SideNav.tsx
+++ frontend/src/components/Portal/Navbar/SideNav.tsx
@@ -10,14 +10,10 @@ import {
   ListAlt,
 } from "@mui/icons-material/";
 import logo from "../../../assets/chatbot-logo.png";
-import Folder from "./Folder";
 import { Link } from "react-router-dom";
 import { Dispatch, SetStateAction, useState } from "react";
 import CreateDialog from "../CreateDialog";
 import { tabs } from "../../../utils/constants";
-import { useSelector } from "react-redux";
-import { useDispatch } from "react-redux";
-import { createFolder, selectFolder } from "../../../store/actions";
 
 const SideWrapper = styled(Box)({
   position: "fixed",
@@ -28,15 +24,16 @@ const SideWrapper = styled(Box)({
   padding: "20px 25px",
   color: " #cbd6e299",
   zIndex: "900",
-  display: 'flex',
-  flexDirection: 'column'
+  display: "flex",
+  flexDirection: "column",
 });
-const Logo = styled(Box)({
+const Logo = styled(Link)({
   display: "flex",
   alignItems: "center",
   gap: "20px",
   paddingBottom: "20px",
   cursor: "pointer",
+  textDecoration: "none",
 });
 const NavBox = styled(Box)({
   display: "flex",
@@ -72,13 +69,8 @@ type Props = {
 export default function SideNav(props: Props) {
   const [open, setOpen] = useState(false);
   const [Tab, setTab] = useState(window.location.hash.slice(9));
-  const folder: FolderDetails[] = useSelector(
-    (state: State) => state.folderList
-  );
-  const dispatch = useDispatch();
-  const handleFolder = (folder: FolderDetails) => {
-    dispatch(selectFolder(folder.id));
-    dispatch(createFolder(folder));
+  const handleNew = () => {
+    console.log("Create new button clicked");
   };
   const handleClose = () => {
     setOpen((open) => !open);
@@ -88,7 +80,7 @@ export default function SideNav(props: Props) {
   };
   return (
     <SideWrapper>
-      <Logo>
+      <Logo to="/">
         <img src={logo} width="36px" />
         <Typography variant="body1" fontWeight={600} color="#fff" pt={1}>
           AI Builder
@@ -105,106 +97,104 @@ export default function SideNav(props: Props) {
       >
         Create New
       </FilledButton>
-      <Box mt={2.5} mb={1.5}>
-        <Folder list={folder} />
-      </Box>
-      <Box overflow="scroll">
-      <NavBox mb={2}>
-        <CustomBox
-          to="/portal/bot"
-          className={`${Tab === "bot" ? "active" : ""}`}
-          onClick={() => handleTabs("bot")}
-        >
-          {props.show ? (
+
+      <Box overflow="scroll" mt={5}>
+        <NavBox mb={4}>
+          <CustomBox
+            to="/portal/bot"
+            className={`${Tab === "bot" ? "active" : ""}`}
+            onClick={() => handleTabs("bot")}
+          >
+            {props.show ? (
+              <>
+                <ListAlt fontSize="small" />
+                <Typography variant="subtitle1" fontWeight={600} pt={0.5}>
+                  Your Bots
+                </Typography>
+              </>
+            ) : (
+              <>
+                <img src={logo} width={24} height={24} />
+                <Typography variant="subtitle1" fontWeight={600} pt={0.5}>
+                  Chatbot builder
+                </Typography>
+              </>
+            )}
+          </CustomBox>
+
+          {props?.show && (
             <>
-              <ListAlt fontSize="small" />
-              <Typography variant="subtitle1" fontWeight={600} pt={0.5}>
-                Your Bots
-              </Typography>
-            </>
-          ) : (
-            <>
-              <img src={logo} width={24} height={24} />
-              <Typography variant="subtitle1" fontWeight={600} pt={0.5}>
-                Chatbot builder
-              </Typography>
+              <Box
+                style={{
+                  cursor: "pointer",
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "10px",
+                  padding: "6px 12px",
+                }}
+              >
+                <Description fontSize="small" />
+                <Typography variant="subtitle1" fontWeight={600} pt={0.5}>
+                  {props?.name}
+                </Typography>
+              </Box>
+              <NavBox ml={2} gap="8px">
+                {tabs.map((tab) => (
+                  <Box
+                    style={{ cursor: "pointer", padding: "5px 16px" }}
+                    onClick={() => {
+                      props.setTabId?.(() => tab.id);
+                      handleTabs(tab.id);
+                    }}
+                    className={`${Tab === tab.id ? "active" : ""}`}
+                    key={tab.id}
+                  >
+                    <Typography variant="subtitle1" fontWeight={600} pt={0.5}>
+                      {tab.name}
+                    </Typography>
+                  </Box>
+                ))}
+              </NavBox>
             </>
           )}
-        </CustomBox>
+        </NavBox>
 
-        {props?.show && (
-          <>
-            <Box
-              style={{
-                cursor: "pointer",
-                display: "flex",
-                alignItems: "center",
-                gap: "10px",
-                padding: "6px 12px",
-              }}
-            >
-              <Description fontSize="small" />
-              <Typography variant="subtitle1" fontWeight={600} pt={0.5}>
-                {props?.name}
-              </Typography>
-            </Box>
-            <NavBox ml={2} gap="8px">
-              {tabs.map((tab) => (
-                <Box
-                  style={{ cursor: "pointer", padding: "5px 16px" }}
-                  onClick={() => {
-                    props.setTabId?.(() => tab.id);
-                    handleTabs(tab.id);
-                  }}
-                  className={`${Tab === tab.id ? "active" : ""}`}
-                  key={tab.id}
-                >
-                  <Typography variant="subtitle1" fontWeight={600} pt={0.5}>
-                    {tab.name}
-                  </Typography>
-                </Box>
-              ))}
-            </NavBox>
-          </>
-        )}
-      </NavBox>
-
-      <OutlinedButton variant="outlined" className="side-nav-btn" fullWidth>
-        <LightbulbCircle />
-        <Typography variant="subtitle1" fontWeight={600}>
-          Learning Center
-        </Typography>
-      </OutlinedButton>
-
-      <FilledButton variant="contained" className="side-nav-btn" fullWidth>
-        <Typography variant="subtitle1" fontWeight={600}>
-          Upgrade
-        </Typography>
-        <Bolt />
-      </FilledButton>
-
-      <NavBox mt={4}>
-        <CustomBox
-          to="/portal/resources"
-          className={`${Tab === "resources" ? "active" : ""}`}
-          onClick={() => handleTabs("resources")}
-        >
-          <StickyNote2 />
-          <Typography variant="body1" fontWeight={600} pt={0.25}>
-            Resources
+        <OutlinedButton variant="outlined" className="side-nav-btn" fullWidth>
+          <LightbulbCircle />
+          <Typography variant="subtitle1" fontWeight={600}>
+            Learning Center
           </Typography>
-        </CustomBox>
-        <CustomBox
-          to="/portal/earn"
-          className={`${Tab === "earn" ? "active" : ""}`}
-          onClick={() => handleTabs("earn")}
-        >
-          <Paid />
-          <Typography variant="body1" fontWeight={600} pt={0.25}>
-            Earn with ChatBuilder
+        </OutlinedButton>
+
+        <FilledButton variant="contained" className="side-nav-btn" fullWidth>
+          <Typography variant="subtitle1" fontWeight={600}>
+            Upgrade
           </Typography>
-        </CustomBox>
-      </NavBox>
+          <Bolt />
+        </FilledButton>
+
+        <NavBox mt={4}>
+          <CustomBox
+            to="/portal/resources"
+            className={`${Tab === "resources" ? "active" : ""}`}
+            onClick={() => handleTabs("resources")}
+          >
+            <StickyNote2 />
+            <Typography variant="body1" fontWeight={600} pt={0.25}>
+              Resources
+            </Typography>
+          </CustomBox>
+          <CustomBox
+            to="/portal/earn"
+            className={`${Tab === "earn" ? "active" : ""}`}
+            onClick={() => handleTabs("earn")}
+          >
+            <Paid />
+            <Typography variant="body1" fontWeight={600} pt={0.25}>
+              Earn with ChatBuilder
+            </Typography>
+          </CustomBox>
+        </NavBox>
       </Box>
       {open && (
         <CreateDialog
@@ -213,8 +203,7 @@ export default function SideNav(props: Props) {
           title="New Project"
           text="Project Name"
           placeholder="Enter the name of project"
-          handleList={handleFolder}
-          folder
+          handleList={handleNew}
         />
       )}
     </SideWrapper>
