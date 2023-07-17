97cb3d1c0038a7692990949ef9dc1a6d3c5fab9a
santushtisharma10 <sharmasantushti1012@gmail.com>
2023-07-18 00:09:14 +0530

    [modification]: types defined

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -2,12 +2,28 @@
 declare module "swiper";
 declare module "aos";
 declare module "react-router-hash-link";
+
 interface BotCardDetails {
   id: string;
   name: string;
 }
 
+type UserDetails = {
+  _id?: string
+  username: string;
+  email: string;
+  password: string;
+  role: string
+  data: Object[]
+}
+
+type User = {
+  access_token: string;
+  user: UserDetails
+}
+
 type State = {
+  userDetails: User | null;
   isUserLoggedIn: boolean;
   isAdmin: boolean;
   botList: BotCardDetails[];
